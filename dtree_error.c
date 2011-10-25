9a793fd58a7e0a767a645275f4bfb222e365a631
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-10-26 01:11:53 +0200

    Added internal error handling module.

---
+++ dtree_error.c
@@ -0,0 +1,48 @@
+#include "dtree.h"
+#include <errno.h>
+#include <string.h>
+
+/**
+ * Error state.
+ */
+static int error = 0;
+
+/**
+ * Holds errno. It is valid when error
+ * is less then zero.
+ */
+static int xerrno = 0;
+
+#define ERRSTR_COUNT (sizeof(errstr)/sizeof(char *))
+static const char *errstr[] = {
+	"Successful"
+};
+
+void dtree_internal_clrerr(void)
+{
+	error  = 0;
+	xerrno = 0;
+}
+
+void dtree_internal_seterr(int e)
+{
+	error  = e;
+	xerrno = errno;
+}
+
+int dtree_iserror(void)
+{
+	return error != 0;
+}
+
+const char *dtree_errstr(void)
+{
+	if(error >= 0 && error < ERRSTR_COUNT)
+		return errstr[error];
+
+	if(error < 0)
+		return strerror(errno);
+
+	return "Unknown error occured";
+}
+
---
+++ dtree_error.h
@@ -0,0 +1,14 @@
+/**
+ * Internal error handling module.
+ * Non-public API.
+ * Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
+ */
+
+#ifndef DTREE_ERROR
+#define DTREE_ERROR
+
+void dtree_internal_clrerr(void);
+void dtree_internal_seterr(int e);
+
+#endif
+
