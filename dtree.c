64607dca5ef9d7315ed2db525c1dcd16629dee88
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-10-26 17:02:17 +0200

    Implemented function dtree_byname

--- dtree.c
+++ dtree.c
@@ -2,6 +2,8 @@
 #include "dtree_error.h"
 #include "dtree_procfs.h"
 
+#include <string.h>
+
 int dtree_open(const char *rootd)
 {
 	int err = dtree_procfs_open(rootd);
@@ -32,5 +34,20 @@ void dtree_dev_free(struct dtree_dev_t *dev)
 int dtree_reset(void)
 {
 	return dtree_procfs_reset();
+}
+
+struct dtree_dev_t *dtree_byname(const char *name)
+{
+	struct dtree_dev_t *curr = NULL;
+
+	if(name == NULL || strlen(name) == 0)
+		return NULL;
+
+	while((curr = dtree_next()) != NULL) {
+		if(!strcmp(name, curr->name))
+			break;
+	}
+
+	return curr;
 }
 
