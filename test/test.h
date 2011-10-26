fd07f8d3863cf7f6a8891e835593de8b091b2cb8
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-10-26 11:12:07 +0200

    Testing help functions were extended

--- test/test.h
+++ test/test.h
@@ -10,5 +10,23 @@ void _test_fail(const char *func, const char *file, int lineno, const char *desc
 }
 #define test_fail(desc) _test_fail(__func__, __FILE__, __LINE__, (desc))
 
+void _test_warn(const char *func, const char *file, int lineno, const char *desc)
+{
+	fprintf(stderr, "Warning in %s (%s:%d)\n", func, file, lineno);
+	fprintf(stderr, "%s\n", desc);
+}
+#define test_warn(desc) _test_warn(__func__, __FILE__, __LINE__, (desc))
+
+#define fail_on_true(t, msg) \
+	if((t) != 0) {test_fail((msg)); return;}
+
+#define fail_on_false(f, msg)     fail_on_true(!(f), (msg))
+#define fail_on_success(err, msg) fail_on_true((err) == 0, (msg))
+#define fail_on_error(t, msg)     fail_on_true((err) != 0, (msg))
+
+#define warn_on_true(t, msg) \
+	if((t) != 0) {test_warn((msg));}
+#define warn_on_false(f, msg)     warn_on_true(!(f), (msg))
+
 #endif
 
