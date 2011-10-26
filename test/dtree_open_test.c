c94d7fceffa5f1f2bb1e7ae1bad89085772923a5
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-10-26 11:12:30 +0200

    Tests of dtree_open() were extended

--- test/dtree_open_test.c
+++ test/dtree_open_test.c
@@ -4,28 +4,22 @@
 void test_pass_null(void)
 {
 	int err = dtree_open(NULL);
-	if(err == 0)
-		test_fail("Successful when passing NULL");
-	if(!dtree_iserror())
-		test_fail("Error is not indicated by dtree_iserror()");
+	fail_on_success(err, "Successful when passing NULL");
+	fail_on_false(dtree_iserror(), "Error is not indicated by dtree_iserror()");
 }
 
 void test_nonexistent_dir(void)
 {
 	int err = dtree_open("/xxx/yyy/zzz");
-	if(err == 0)
-		test_fail("Successful when passing non-existent dir: /xxx/yyy/zzz");
-	if(!dtree_iserror())
-		test_fail("Error is not indicated by dtree_iserror()");
+	fail_on_success(err, "Successful when passing non-existent dir: /xxx/yyy/zzz");
+	fail_on_false(dtree_iserror(), "Error is not indicated by dtree_iserror()");
 }
 
 void test_pass_invalid(void)
 {
 	int err = dtree_open(__FILE__);
-	if(err == 0)
-		test_fail("Successful when passing " __FILE__ " as root dir");
-	if(!dtree_iserror())
-		test_fail("Error is not indicated by dtree_iserror()");
+	fail_on_success(err, "Successful when passing " __FILE__ " as root dir");
+	fail_on_false(dtree_iserror(), "Error is not indicated by dtree_iserror()");
 }
 
 /**
@@ -35,14 +29,39 @@ void test_pass_invalid(void)
 void test_pass_mostly_valid(void)
 {
 	int err = dtree_open("/proc/device-tree");
-	if(err != 0) {
-		test_fail("Open of /proc/device-tree failed, is it present on the system?");
-		return;
-	}
-	if(dtree_iserror())
-		test_fail("Error is indicated by dtree_iserror(), but should not be");
-
+	fail_on_error(err, "Open of /proc/device-tree failed, is it present on the system?");
+	warn_on_true(dtree_iserror(), "Error is indicated by dtree_iserror(), but should not be");
 	dtree_close();
+}
+
+/**
+ * Gets information from the testing device-tree
+ * in current directory.
+ */
+void test_open_test_dtree(void)
+{
+	int err = dtree_open("device-tree");
+	fail_on_error(err, "Can not open testing device-tree");
+	warn_on_true(dtree_iserror(), "Error state is set, but should not be");
+	dtree_close();
+}
+
+/**
+ * Tests whether the error flag is cleared in correct way.
+ */
+void test_clear_error(void)
+{
+	int err = dtree_open(NULL);
+	fail_on_success(err, "Open of NULL was successful");
+
+	warn_on_false(dtree_iserror(), "No error is indicated");
+	dtree_close();
+
+	fail_on_false(dtree_iserror(), "The error was cleared after close");
+
+	err = dtree_open("device-tree");
+	fail_on_error(err, "Can not open testing device-tree");
+	fail_on_true(dtree_iserror(), "The error was not cleared");
 }
 
 int main(void)
@@ -50,6 +69,8 @@ int main(void)
 	test_pass_null();
 	test_nonexistent_dir();
 	test_pass_invalid();
+	test_open_test_dtree();
 	test_pass_mostly_valid();
+	test_clear_error();
 }
 
