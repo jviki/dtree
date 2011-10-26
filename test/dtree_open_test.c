99de9da3715cb14014b02d6711e9fa37f1c92988
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-10-26 12:09:59 +0200

    Using test_start/end when testing dtree_open

--- test/dtree_open_test.c
+++ test/dtree_open_test.c
@@ -3,23 +3,29 @@
 
 void test_pass_null(void)
 {
+	test_start();
 	int err = dtree_open(NULL);
 	fail_on_success(err, "Successful when passing NULL");
 	fail_on_false(dtree_iserror(), "Error is not indicated by dtree_iserror()");
+	test_end();
 }
 
 void test_nonexistent_dir(void)
 {
+	test_start();
 	int err = dtree_open("/xxx/yyy/zzz");
 	fail_on_success(err, "Successful when passing non-existent dir: /xxx/yyy/zzz");
 	fail_on_false(dtree_iserror(), "Error is not indicated by dtree_iserror()");
+	test_end();
 }
 
 void test_pass_invalid(void)
 {
+	test_start();
 	int err = dtree_open(__FILE__);
 	fail_on_success(err, "Successful when passing " __FILE__ " as root dir");
 	fail_on_false(dtree_iserror(), "Error is not indicated by dtree_iserror()");
+	test_end();
 }
 
 /**
@@ -28,10 +34,12 @@ void test_pass_invalid(void)
  */
 void test_pass_mostly_valid(void)
 {
+	test_start();
 	int err = dtree_open("/proc/device-tree");
 	fail_on_error(err, "Open of /proc/device-tree failed, is it present on the system?");
 	warn_on_true(dtree_iserror(), "Error is indicated by dtree_iserror(), but should not be");
 	dtree_close();
+	test_end();
 }
 
 /**
@@ -40,10 +48,12 @@ void test_pass_mostly_valid(void)
  */
 void test_open_test_dtree(void)
 {
+	test_start();
 	int err = dtree_open("device-tree");
 	fail_on_error(err, "Can not open testing device-tree");
 	warn_on_true(dtree_iserror(), "Error state is set, but should not be");
 	dtree_close();
+	test_end();
 }
 
 /**
@@ -51,6 +61,7 @@ void test_open_test_dtree(void)
  */
 void test_clear_error(void)
 {
+	test_start();
 	int err = dtree_open(NULL);
 	fail_on_success(err, "Open of NULL was successful");
 
@@ -62,6 +73,7 @@ void test_clear_error(void)
 	err = dtree_open("device-tree");
 	fail_on_error(err, "Can not open testing device-tree");
 	fail_on_true(dtree_iserror(), "The error was not cleared");
+	test_end();
 }
 
 int main(void)
