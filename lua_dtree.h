6ad1339c0e2317753c2b20288be827409a1a8fed
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-06-02 12:14:47 +0200

    Interface to the native Lua dtree module

---
+++ lua_dtree.h
@@ -0,0 +1,15 @@
+/**
+ * lua_dtree.h
+ * Copyright (C) 2012 Jan Viktorin
+ */
+
+#ifndef LUA_DTREE_H
+#define LUA_DTREE_H
+
+/**
+ * Registers itself inside the Lua engine.
+ */
+int luaopen_dtreelib(lua_State *l);
+
+#endif
+
